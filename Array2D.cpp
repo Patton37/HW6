ce9f67da3106bf84e2aa5b37a0941ac20f4875e2
Patton37 <51462416+Patton37@users.noreply.github.com>
2021-03-02 19:01:21 -0500

    Add files via upload

--- Array2D.cpp
+++ Array2D.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <cstdlib>
-#include "Array2D.H"
+#include "Array2D.h"
 
 // The character parameter exists to give this a different signature than any of 
 // the public constructors.
 Array2D::Array2D(int numR, int numC, char differentiator) : numRows(numR), numCols(numC) {
 
-   ary = new int*[numRows];
+   int** ary = new int*[numRows];
    for (int i = 0; i < numRows; i++) {
       ary[i] = new int[numCols];
       for (int j = 0; j < numCols; j++) {
@@ -30,39 +30,6 @@ Array2D::Array2D(int numR, int numC, int val) : Array2D(numR, numC, 'c') {
       }
    }
 }
-
-Array2D::Array2D(const Array2D& other) : Array2D(numR, numC, 'c') // copy constructor            #1
-{
-   int cols = Array2D.getNumCols(); 
-   int rows = Array2D.getNumRows();
-
-   for (int i = 0; i < numRows; i++) {
-      for (int j = 0; j < numCols; j++) {
-         ary[i][j] = i;
-      }
-   }
-
-}
-
-int getNumRows()                                                 //Getters for rows and cols     #2 
-{
-   return numRows; 
-}
-
-int getNumCols()                                                
-{
-   return numCols; 
-}                                                                //end                           
-
-
-Array2D::~Array2D()                                              //Destructor for Array2D        #3
-{
-   deleteAry(this.Array2D)
-} 
-
-
-
-
 int& Array2D::operator( )(int row, int col) {
    return ary[row][col];
 }
@@ -81,3 +48,120 @@ void Array2D::deleteAry(int** ary) {
    }
    delete [ ] ary;
 }
+
+//__________________________________________________________________________________________
+// _o-^-o
+//------------------------------------------------------------------------------------------
+//__________________________________________________________________________________________
+
+Array2D::Array2D(const Array2D& other) : Array2D(numR, numC, 'c') // copy constructor            #1
+{
+   int cols = ar1.getNumCols(); 
+   int rows = ar2.getNumRows();
+
+   for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
+         ary[i][j] = i;
+      }
+   }
+
+}
+
+int Array2D::getNumRows()                                                 //Getters for rows and cols     #2 
+{
+   return numRows; 
+}
+
+int Array2D::getNumCols()                                                
+{
+   return numCols; 
+}                                                                //end                           
+
+
+Array2D::~Array2D()                                              //Destructor for Array2D        #3
+{
+for(int i = 0 ; i < numRows ; i++)
+{
+   delete[] ary[i]
+}
+delete[] ary; 
+} 
+
+Array2D& Array2D::operator= (const Array2D& ar1, const Array2D& ar2)
+   {
+
+   int rows = ar1->getNumRows();
+   int cols = ar1->getNumCols();
+   Array2D *arNew = new Array2D(rows,cols); //Temp array
+   
+   	for (int i=0 ; i< rows ; i++){
+		   for (int j=0 ; j<cols ;  j++){
+             ar1[i][j] = ar2[i][j]; 
+         }
+      }
+   return storage;
+   }
+   
+
+  bool Array2D::operator== (const Array2D& ar1, const Array2D& ar2)
+   {
+      bool tf = 0; 
+      int rows = ar1->getNumRows();
+      int cols = ar1->getNumCols();
+   	for (int i=0 ; i< rows ; i++){
+		   for (int j=0 ; j<cols ;  j++){
+            if(ar1[i][j] != ar2[i][j])
+            {
+               return(0);
+            }
+         }
+      }
+      return(1); 
+   }
+
+   //Overload * operator to perform 2D matrix multiplication and return the array filled with -1's 
+   Array2D& Array2D::operator* (const Array2D& ar1, const Array2D& ar2)
+   {
+      int rows = ar1->getNumRows();
+      int cols = ar2->getNumCols();
+
+      Array2D  *boyo =  new Array2D(rows,cols); 
+
+      if (rows != cols)
+      {
+         printf("ERROR IN operator*(const Array2D& ar1, const Array2D& ar2):        1st Array Rows != 2nd Array Cols");
+             
+      }
+      else
+      {
+         for( int i = 0 ; i < rows ; i++ )
+         {
+            for(int k = 0 ; k < cols ; k++ ) {
+            boyo[i][k] = -1; 
+            }
+         }
+         return(*boyo); 
+      }
+
+      return nullptr; 
+   
+   }
+
+   std::ostream& Array2D::operator<<(std::ostream&, const Array2D& ar2)
+   {
+      int rows = ar2->getNumRows();
+      int cols = ar2->getNumCols();
+   	for (int i=0 ; i< rows ; i++){
+		   for (int j=0 ; j<cols ;  j++){
+            std::cout << ar2[i][j] << " " ; 
+         }
+         std::cout << "\n"; 
+      }
+   }
+
+
+
+
+
+
+
--- Array2D.h
+++ Array2D.h
@@ -8,18 +8,32 @@ private:
    bool sameSize(const Array2D& other) const;
    
     //New stuff
-   Array2D(const Array2D& other); // copy constructor 
+    
+   
+
 
 public:
    Array2D(int numR, int numC); 
    Array2D(int numR, int numC, int val); 
+   Array2D(const Array2D& other); // copy constructor
 
    virtual int& operator( )(int row, int col);
    virtual int operator( )(int row, int col) const;
    //New stuff
+   int numRows; 
+   int numCols;
+   
+   
    int getNumRows();  
    int getNumCols(); 
-   ~Array2D(); 
+   virtual~Array2D(); 
+   
+   //OP OVERLOADS I ADDED 
+   Array2D& operator= (const Array2D& ar2);
+   Array2D& operator* (const Array2D& ar2);
+
+   bool operator==(const Array2D& ar2); 
+   std::ostream& operator<<(Array2D& ar1); 
 
 };
 #endif /* ARRAY2D_H_ */
